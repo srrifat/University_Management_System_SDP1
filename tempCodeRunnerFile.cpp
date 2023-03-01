038038188b5146c8e3bbcf154d86f87fa16e8c5d
Saydur Rahman Rifat <61623902+srrifat@users.noreply.github.com>
2023-03-01 11:42:39 +0600

    SDP-1

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,78 @@
+
+void strg_display_record();
+void strg_serch_record();
+void strg_del_record();
+
+void St_result();
+void stre_insert_record();
+void stre_display_record();
+void stre_serch_record();
+void stre_del_record();
+
+//bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb student (user) bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+void st_login(void);
+void s_user(void);
+void s_profile(void);
+void class_routine(void);
+void fac_information(void);
+
+void strg_login();
+void registration(void);
+void s_lib_login();
+void s_lib(void);
+void payment(void);
+void py_insert_record();
+void py_display_record();
+void bikash(void);
+void weblink(void);
+
+void nogod(void);
+void rocket(void);
+void all_course(void);
+
+void result(void);
+void stre_login(void);
+
+void career_guideline(void);
+void s_tra_login(void);
+void s_tra(void);
+//333333333333333333333333333333333333333333333333333333333333333 teacher assistant (admin) 333333333333333333333333333333333333333333333333333333333333333333
+void Te_as(void);
+void ta_insert_record();
+void ta_display_record();
+void ta_serch_record();
+void ta_del_record();
+
+//cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc teacher assistant (user) ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+void te_as(void);
+void ta_login(void);
+void ta_profile(void);
+void ta_research(void);
+//4444444444444444444444444444444444444444444444444444444444444 employ(admin) 4444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+void Em(void);
+void em_insert_record(void);
+void em_display_record(void);
+void em_search_record(void);
+void em_del_record(void);
+
+//ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd employ (user) ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+void emp(void);
+void em_login();
+void em_profile();
+void em_registation();
+void account_section();
+
+
+//55555555555555555555555555555555555555555555555555555555555555 Graduated (admin) 55555555555555555555555555555555555555555555555555555555555555555555555555
+void Gr(void);
+void gr_insert_record();
+void gr_display_record();
+void gr_serch_record();
+void gr_del_record();
+
+//eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee graduated (user) eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+void gra(void);
+void gra_login();
+void gra_profile();
+void gra_program();
+void gra_group();
